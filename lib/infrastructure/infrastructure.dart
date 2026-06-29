0c017ab82b6903d65ee7317d6c2cede0725e08f0
YagoSchramm <yrtoiu1515@gmail.com>
2026-06-29 18:12:35 -0300

    feat(): Add the user repository and usecase to the app init

--- lib/infrastructure/infrastructure.dart
+++ lib/infrastructure/infrastructure.dart
@@ -1,13 +1,17 @@
 import 'package:sprinter/domain/usecases/authentication.dart';
 import 'package:sprinter/domain/usecases/impl/authentication_use_case.dart';
+import 'package:sprinter/domain/usecases/impl/user_use_case.dart';
 import 'package:sprinter/domain/usecases/product.dart';
 import 'package:sprinter/domain/usecases/impl/product_use_case.dart';
+import 'package:sprinter/domain/usecases/user.dart';
 import 'package:sprinter/infrastructure/repositories/impl/authentication_repository.dart';
 import 'package:sprinter/infrastructure/repositories/impl/product_repository.dart';
+import 'package:sprinter/infrastructure/repositories/impl/user_repositroy.dart';
 import 'package:sprinter/infrastructure/storage/impl/secure_storage.dart';
 
 late final AuthenticationUseCase authenticationUseCase;
 late final ProductUseCase productUseCase;
+late final UserUseCase userUseCase;
 
 void initialize() {
   // App storage
@@ -16,8 +20,10 @@ void initialize() {
   // Repositories
   final authenticationRepository = newAuthenticationRepository(secureStorage);
   final productsRepository = newProductRepository(secureStorage);
+  final userRepository = newUserRepository(secureStorage);
 
   // Use cases
   authenticationUseCase = newAuthenticationUseCase(authenticationRepository);
   productUseCase = newProductUseCase(productsRepository);
+  userUseCase = newUserUseCase(userRepository);
 }
